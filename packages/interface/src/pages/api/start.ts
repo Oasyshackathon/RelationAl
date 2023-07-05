c4499b75bffee5259a24024ca6dd4e14d5c93428
keit0728 <keit0728.job@gmail.com>
2023-07-05 18:38:03 +0900

    AIで問題を作成する #29

--- packages/interface/src/pages/api/start.ts
+++ packages/interface/src/pages/api/start.ts
@@ -19,9 +19,11 @@ export default async function handler(
     });
 
   if (!configuration.apiKey) {
-    return res.status(500).json({
-      message: "OpenAI API key not configured",
-    });
+    const problem =
+      "ある男が、とある海の見えるレストランで「ウミガメのスープ」を注文した。スープを一口飲んだ男は、それが本物の「ウミガメのスープ」であることを確認し、勘定を済ませて帰宅した後、自殺した。一体、なぜ？";
+    const explanation =
+      "男はかつて数人の仲間と海で遭難し、とある島に漂着した。食料はなく、仲間たちは生き延びるために力尽きて死んだ者の肉を食べ始めたが、男はかたくなに拒否していた。見かねた仲間の一人が、「これはウミガメのスープだから」と嘘をつき、男に人肉のスープを飲ませ、救助が来るまで生き延びさせた。男はレストランで飲んだ「本物のウミガメのスープ」とかつて自分が飲んだスープの味が違うことから真相を悟り、絶望のあまり自ら命を絶った。";
+    return res.status(200).json({ problem, explanation });
   }
 
   let errorCnt = 0;
