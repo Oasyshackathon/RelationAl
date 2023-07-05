175e3e114e4ceb915f6e87159a9c6309cf137f03
内田祥喜 <yoshikiuchida@uchidasachikinoMacBook-Air.local>
2023-07-06 00:01:18 +0900

    page6#7

---
+++ packages/interface/src/pages/page6.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components/layouts/Header";
+import { Main } from "@/components/layouts/Main";
+import { MainPage6 } from "@/components/layouts/Main/MainPage6";
+
+export default function page6() {
+  return (
+    <>
+      <Header />
+      <Main>
+        <MainPage6 />
+      </Main>
+    </>
+  );
+}
