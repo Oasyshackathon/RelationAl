ab8f76b49b5ca7e3982d1739542d386446bcf157
keit0728 <keit0728.job@gmail.com>
2023-07-05 18:32:05 +0900

    AIで問題を作成する #29

---
+++ packages/interface/src/const/error.ts
@@ -0,0 +1,2 @@
+export const MAX_ERROR_CNT = 5;
+export const ERROR_WAIT_TIME = 1000; // ms
--- packages/interface/src/pages/api/start.ts
+++ packages/interface/src/pages/api/start.ts
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { MAX_ERROR_CNT } from "@/const/error";
+import { getLTPuzzlePrompt } from "@/utils/prompt";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,11 +18,42 @@ export default async function handler(
       message: "Only POST",
     });
 
-  // TODO: #29 OpenAI APIで問題を作成する
-  const problem =
-    "ある男が、とある海の見えるレストランで「ウミガメのスープ」を注文した。スープを一口飲んだ男は、それが本物の「ウミガメのスープ」であることを確認し、勘定を済ませて帰宅した後、自殺した。一体、なぜ？";
-  const explanation =
-    "男はかつて数人の仲間と海で遭難し、とある島に漂着した。食料はなく、仲間たちは生き延びるために力尽きて死んだ者の肉を食べ始めたが、男はかたくなに拒否していた。見かねた仲間の一人が、「これはウミガメのスープだから」と嘘をつき、男に人肉のスープを飲ませ、救助が来るまで生き延びさせた。男はレストランで飲んだ「本物のウミガメのスープ」とかつて自分が飲んだスープの味が違うことから真相を悟り、絶望のあまり自ら命を絶った。";
+  if (!configuration.apiKey) {
+    return res.status(500).json({
+      message: "OpenAI API key not configured",
+    });
+  }
+
+  let errorCnt = 0;
+  const prompt = getLTPuzzlePrompt();
+  let json;
+  while (true) {
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+        temperature: 1.0,
+      });
+      console.log(completion.data);
+      console.log(completion.data.choices);
+      console.log(completion.data.usage);
+      json = JSON.parse(completion.data.choices[0].message!.content!);
+      errorCnt = 0;
+      break;
+    } catch (error) {
+      errorCnt++;
+      error instanceof Error
+        ? console.error(error.message)
+        : console.error(error);
+      if (errorCnt >= MAX_ERROR_CNT) {
+        if (error instanceof Error)
+          return res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error });
+      }
+    }
+  }
+  const problem = json.problem;
+  const explanation = json.explanation;
 
   return res.status(200).json({ problem, explanation });
 }
