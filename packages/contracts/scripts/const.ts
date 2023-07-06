d3cbca09bf0748d8f753303a3ab08faa5bd7aba6
keit0728 <keit0728.job@gmail.com>
2023-07-07 03:51:08 +0900

    vercelへホスティングサービスを変更

--- packages/contracts/scripts/const.ts
+++ packages/contracts/scripts/const.ts
@@ -6,4 +6,4 @@ export const LT_PUZZLE_PROXY_ADDRESS =
   "0xE1999dfB74e0eaDFCf89C031430Bb1EcB87E7958"; // sandVerse
 // "0x4b4d9cb4A9975Daab2438B79Aab8A702B932C3c5"; // sandVerse
 
-export const LT_PUZZLE_EXTERNAL_LINK = "https://relational.netlify.app/";
+export const LT_PUZZLE_EXTERNAL_LINK = "https://relational.vercel.app/";
